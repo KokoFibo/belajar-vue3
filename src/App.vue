03bd6aeed1e6f43720c80d4c6cc6e5ab516262f0
KokoFibo <kokonacci@gmail.com>
2022-10-02 12:20:04 +0700

    trim

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     <div class="col-4">
       <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label">Name</label>
-        <input v-model="formValues.name" type="text" class="form-control" id="exampleFormControlInput1" />
+        <input v-model.trim="formValues.name" type="text" class="form-control" id="exampleFormControlInput1" />
       </div>
 
       <div class="mb-3">
